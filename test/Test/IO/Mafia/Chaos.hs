66e1457e89b4c7fff3ad8e35e1fee3a780acc5d1
Jacob Stanley <jacob@stanley.io>
2015-10-16 11:35:21 +1100

    Expanded the chaos test to cover many more cases

--- test/Test/IO/Mafia/Chaos.hs
+++ test/Test/IO/Mafia/Chaos.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -7,13 +8,17 @@
 module Test.IO.Mafia.Chaos where
 
 import           Control.Exception (IOException)
-import           Control.Monad.Catch
+import           Control.Monad.Catch (MonadCatch(..), bracket, handle)
 import           Control.Monad.IO.Class (MonadIO(..))
 
-import           Disorder.Corpus
-import           Disorder.Core.IO
+import           Disorder.Corpus (muppets, viruses)
+import           Disorder.Core.IO (testIO)
 
 import           Data.Char (toUpper)
+import           Data.Map (Map)
+import qualified Data.Map as Map
+import           Data.Set (Set)
+import qualified Data.Set as Set
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -27,25 +32,220 @@ import           P
 import           System.IO (IO, print)
 import           System.IO.Temp (withSystemTempDirectory)
 
-import           Test.QuickCheck
+import           Test.QuickCheck (Arbitrary(..), Gen, Property, Testable(..))
+import           Test.QuickCheck (forAllProperties)
+import qualified Test.QuickCheck as QC
 
-import           X.Control.Monad.Trans.Either
+import           X.Control.Monad.Trans.Either (EitherT(..))
 
 ------------------------------------------------------------------------
 
-data Action =
-    Build
-  | AddLib    Text
-  | RemoveLib Text
+type PackageName   = Text
+type SubmoduleName = Text
+
+data Repo = Repo {
+    repoFocusPackage  :: PackageDeps
+  , repoOtherPackages :: Map PackageName PackageDeps
+  } deriving (Eq, Ord, Show)
+
+data PackageDeps = PackageDeps {
+    depSubmodules    :: Set SubmoduleName
+  , depLocalPackages :: Set PackageName
+  } deriving (Eq, Ord, Show)
+
+newtype Actions = Actions { unActions :: [Action] }
   deriving (Eq, Ord, Show)
 
-instance Arbitrary Action where
-  arbitrary = oneof [ AddLib    <$> genPackageName
-                    , RemoveLib <$> genPackageName
-                    , pure Build ]
+data Action =
+    Build
+  | AddLocal     PackageName   PackageName
+  | AddSubmodule SubmoduleName PackageName
+  | RemoveLocal     PackageName
+  | RemoveSubmodule SubmoduleName
+  -- Sidestep = temporarily move out the way, try to build, then move back to
+  -- the valid location. Useful to test that caching works even across invalid
+  -- source states.
+  | SidestepLocal     PackageName
+  | SidestepSubmodule SubmoduleName
+  deriving (Eq, Ord, Show)
 
-genPackageName :: Gen Text
-genPackageName = T.replace " " "-" <$> elements viruses
+repoSubmodules :: Repo -> Set SubmoduleName
+repoSubmodules (Repo focus others) =
+  let fs = depSubmodules focus
+      os = Set.unions (fmap depSubmodules (Map.elems others)) in
+  Set.union fs os
+
+emptyRepo :: Repo
+emptyRepo = Repo emptyPackageDeps Map.empty
+
+emptyPackageDeps :: PackageDeps
+emptyPackageDeps = PackageDeps Set.empty Set.empty
+
+fromTestRun :: [(Action, Repo)] -> [Action]
+fromTestRun = fmap fst
+
+fromActions :: [Action] -> Maybe [(Action, Repo)]
+fromActions = fromActions' emptyRepo
+
+fromActions' :: Repo -> [Action] -> Maybe [(Action, Repo)]
+fromActions' _    []       = Just []
+fromActions' repo (a : as) = do
+  repo' <- applyAction a repo
+  xs    <- fromActions' repo' as
+  pure $ (a, repo') : xs
+
+finalRepo :: [(Action, Repo)] -> Repo
+finalRepo = \case
+  []             -> emptyRepo
+  ((_, repo):[]) -> repo
+  (_:xs)         -> finalRepo xs
+
+------------------------------------------------------------------------
+
+instance Arbitrary Actions where
+  arbitrary =
+    fmap (Actions . fromTestRun) . QC.sized $ \n -> do
+      k <- QC.choose (0, n)
+      genTestRun k
+
+  shrink (Actions as) = Actions <$> do
+    as' <- QC.shrinkList shrinkAction as
+    case fromActions as' of
+      Nothing -> pure []
+      Just _  -> pure as'
+
+shrinkAction :: Action -> [Action]
+shrinkAction = \case
+  AddLocal     dep pkg
+   | pkg /= focusPackageName -> [AddLocal     dep focusPackageName]
+   | otherwise               -> []
+  AddSubmodule dep pkg
+   | pkg /= focusPackageName -> [AddSubmodule dep focusPackageName]
+   | otherwise               -> []
+  Build                      -> []
+  RemoveLocal     _          -> []
+  RemoveSubmodule _          -> []
+  SidestepLocal     _        -> []
+  SidestepSubmodule _        -> []
+
+genTestRun :: Int -> Gen [(Action, Repo)]
+genTestRun n
+  | n <= 0    = pure []
+  | otherwise = do
+      testRun <- genTestRun (n - 1)
+
+      let repo = finalRepo testRun
+      action <- genAction (finalRepo testRun)
+
+      pure . fromMaybe testRun
+           . fmap (\r -> testRun <> [(action, r)])
+           $ applyAction action repo
+
+genAction :: Repo -> Gen Action
+genAction repo@(Repo _ others) = do
+  mbLocalPackageNames <- oneOfSet localPackageNames
+  mbSubmoduleNames    <- oneOfSet submoduleNames
+
+  let removableLocals = Map.keysSet others
+      availableLocals = Set.insert focusPackageName
+                      . maybe id Set.delete mbLocalPackageNames
+                      $ removableLocals
+
+  mbRemovableLocals     <- oneOfSet removableLocals
+  mbAvailableLocals     <- oneOfSet availableLocals
+  mbRemovableSubmodules <- oneOfSet (repoSubmodules repo)
+
+  QC.elements $ catMaybes [
+      Just Build
+    , AddLocal          <$> mbLocalPackageNames <*> mbAvailableLocals
+    , AddSubmodule      <$> mbSubmoduleNames    <*> mbAvailableLocals
+    , RemoveLocal       <$> mbRemovableLocals
+    , RemoveSubmodule   <$> mbRemovableSubmodules
+    , SidestepLocal     <$> mbRemovableLocals
+    , SidestepSubmodule <$> mbRemovableSubmodules
+    ]
+
+focusPackageName :: PackageName
+focusPackageName = "chaos-focus"
+
+localPackageNames :: Set PackageName
+localPackageNames = Set.fromList (("chaos-" <>) . T.replace " " "-" <$> viruses)
+
+submoduleNames :: Set SubmoduleName
+submoduleNames = Set.fromList (T.replace " " "-" <$> muppets)
+
+oneOfSet :: Set a -> Gen (Maybe a)
+oneOfSet xs
+  | Set.null xs = pure Nothing
+  | otherwise   = Just <$> QC.elements (Set.toList xs)
+
+applyAction :: Action -> Repo -> Maybe Repo
+applyAction action =
+  let mustModify f g repo = if g repo == repo
+                            then Nothing
+                            else Just (f (repo, g repo))
+  in case action of
+    Build                     -> Just . id
+    AddLocal          dep pkg -> mustModify snd (addLocal        dep pkg)
+    AddSubmodule      dep pkg -> mustModify snd (addSubmodule    dep pkg)
+    RemoveLocal       dep     -> mustModify snd (removeLocal     dep)
+    RemoveSubmodule   dep     -> mustModify snd (removeSubmodule dep)
+    SidestepLocal     dep     -> mustModify fst (removeLocal     dep)
+    SidestepSubmodule dep     -> mustModify fst (removeSubmodule dep)
+
+addLocal :: PackageName -> PackageName -> Repo -> Repo
+addLocal dep pkg repo@(Repo f os)
+  | pkgIsFocus = Repo (addLocal' dep f) os'
+  | isCircular = repo
+  | pkgExists  = Repo f (Map.adjust (addLocal' dep) pkg os')
+  | otherwise  = repo
+  where
+    pkgIsFocus = pkg == focusPackageName
+    isCircular = Set.member pkg depDeps
+    pkgExists  = Map.member pkg os
+
+    depDeps = fromMaybe Set.empty (recursiveLocals repo <$> Map.lookup dep os')
+    os'     = Map.insertWith (\_ old -> old) dep emptyPackageDeps os
+
+addLocal' :: PackageName -> PackageDeps -> PackageDeps
+addLocal' dep (PackageDeps ss ls) =
+  PackageDeps ss (Set.insert dep ls)
+
+recursiveLocals :: Repo -> PackageDeps -> Set PackageName
+recursiveLocals repo@(Repo _ os) (PackageDeps _ ls) =
+  let lookup name = fromMaybe Set.empty (recursiveLocals repo <$> Map.lookup name os) in
+  Set.union (unionMap lookup ls) ls
+
+unionMap :: Ord b => (a -> Set b) -> Set a -> Set b
+unionMap f = Set.foldl (\xs x -> xs `Set.union` f x) Set.empty
+
+addSubmodule :: SubmoduleName -> PackageName -> Repo -> Repo
+addSubmodule dep pkg (Repo f os) =
+  if pkg == focusPackageName
+  then Repo (addSubmodule' dep f) os
+  else Repo f (Map.adjust (addSubmodule' dep) pkg os)
+
+addSubmodule' :: SubmoduleName -> PackageDeps -> PackageDeps
+addSubmodule' dep (PackageDeps ss ls) =
+  PackageDeps (Set.insert dep ss) ls
+
+removeLocal :: PackageName -> Repo -> Repo
+removeLocal dep (Repo f os) =
+  let rm = Map.map (removeLocal' dep) . Map.delete dep in
+  Repo (removeLocal' dep f) (rm os)
+
+removeLocal' :: PackageName -> PackageDeps -> PackageDeps
+removeLocal' dep (PackageDeps ss ls) =
+  PackageDeps ss (Set.delete dep ls)
+
+removeSubmodule :: SubmoduleName -> Repo -> Repo
+removeSubmodule dep (Repo f os) =
+  let rm = Map.map (removeSubmodule' dep) . Map.delete dep in
+  Repo (removeSubmodule' dep f) (rm os)
+
+removeSubmodule' :: SubmoduleName -> PackageDeps -> PackageDeps
+removeSubmodule' dep (PackageDeps ss ls) =
+  PackageDeps (Set.delete dep ss) ls
 
 ------------------------------------------------------------------------
 
@@ -56,7 +256,7 @@ withTempDirectory :: Testable a => (Directory -> EitherT ProcessError IO a) -> P
 withTempDirectory io = testIO . bracketDirectory $ do
   result  <- withSystemTempDirectory "mafia.chaos" (runEitherT . io . T.pack)
   case result of
-    Left  err -> return (counterexample (show err) False)
+    Left  err -> return (QC.counterexample (show err) False)
     Right x   -> return (property x)
 
 hush :: (MonadIO m, MonadCatch m) => a -> m a -> m a
@@ -64,21 +264,33 @@ hush def = handle $ \(_ :: IOException) -> return def
 
 ------------------------------------------------------------------------
 
-writeRootProject :: (MonadIO m, MonadCatch m) => m ()
-writeRootProject = do
-  libs <- hush [] $ getDirectoryContents "lib"
-  writeProject "." "root" libs
+writeRepo :: (MonadIO m, MonadCatch m) => Directory -> Repo -> m ()
+writeRepo dir repo@(Repo focus others) = do
+  writePackage dir repo focusPackageName focus
+  mapM_ (\(name, pkg) -> writePackage dir repo name pkg) (Map.toList others)
 
-writeProject :: MonadIO m => Directory -> Text -> [Text] -> m ()
-writeProject dir name deps = do
-  let src     = dir </> "src"
-      modName = dromedary name
+  let lib = dir </> "lib"
+  mapM_ (\name -> writePackage lib repo name emptyPackageDeps) (repoSubmodules repo)
 
-  writeFile (dir </> name <> ".cabal")
-            (cabalText name deps)
+writePackage :: (MonadIO m, MonadCatch m) => Directory -> Repo -> PackageName -> PackageDeps -> m ()
+writePackage dir repo name pd@(PackageDeps ss ls) = do
+  let deps   = Set.toList (Set.union ss ls)
+      locals = Set.toList (recursiveLocals repo pd)
+  writeProject (dir </> name) name deps locals
 
-  writeFile (src </> modName <> ".hs")
-            ("module " <> modName <> " where\n")
+writeProject :: (MonadIO m, MonadCatch m) => Directory -> Text -> [Text] -> [Text] -> m ()
+writeProject dir name deps locals = do
+  let modName = dromedary name
+      cabalFile = dir </> name <> ".cabal"
+      subsFile  = dir </> name <> ".submodules"
+      hsFile    = dir </> "src" </> modName <> ".hs"
+
+  writeFile cabalFile (cabalText name deps)
+  writeFile hsFile    ("module " <> modName <> " where\n")
+
+  if null locals
+     then removeFile subsFile `catch` \(_ :: IOException) -> return ()
+     else writeFile  subsFile (T.unlines locals)
 
 writeFile :: MonadIO m => Path -> Text -> m ()
 writeFile path txt = do
@@ -112,41 +324,81 @@ cabalText name deps = T.unlines [
 
 ------------------------------------------------------------------------
 
-prop_chaos (actions :: [Action]) = withTempDirectory $ \temp -> do
+prop_chaos (Actions actions) = withTempDirectory $ \temp -> do
   liftIO (print actions)
 
   mafia <- (</> "dist/build/mafia/mafia") <$> getCurrentDirectory
 
-  setCurrentDirectory temp
+  let repoDir   = temp </> "repo"
+      focusDir  = repoDir </> focusPackageName
+
+  createDirectoryIfMissing False repoDir
+  createDirectoryIfMissing False focusDir
+
+  setCurrentDirectory repoDir
   call_ id "git" ["init"]
-  writeRootProject
 
-  forM_ actions $ \action -> do
-    runAction mafia action
+  setCurrentDirectory focusDir
+  writeRepo repoDir emptyRepo
 
-  return True
+  case fromActions actions of
+    Nothing -> do
+      return (QC.counterexample "Invalid set of actions" (property False))
 
-runAction :: File -> Action -> EitherT ProcessError IO ()
-runAction mafia = \case
-  AddLib lib -> do
-    let dir = "lib" </> lib
-    liftIO (T.putStrLn ("+ " <> dir))
+    Just testRun -> do
+      forM_ testRun $ \(action, repo) -> do
+        runAction mafia repoDir repo action
+      return (property True)
 
-    writeProject dir lib []
-    writeRootProject
-
-  RemoveLib lib -> do
-    let dir = "lib" </> lib
-    liftIO (T.putStrLn ("- " <> dir))
-
-    hush () $ removeDirectoryRecursive dir
-    writeRootProject
-
+runAction :: File -> Directory -> Repo -> Action -> EitherT ProcessError IO ()
+runAction mafia repoDir repo = \case
   Build -> do
     liftIO (T.putStrLn "$ mafia build")
     call_ id mafia ["build"]
 
+  AddLocal dep pkg -> do
+    putAdd (dep <> " -> " <> pkg)
+    writeRepo repoDir repo
+
+  AddSubmodule dep pkg -> do
+    let dep' = "lib" </> dep
+    putAdd (dep' <> " -> " <> pkg)
+    writeRepo repoDir repo
+
+  RemoveLocal dep -> do
+    putRemove dep
+    removeDirectoryRecursive (repoDir </> dep)
+    writeRepo repoDir repo
+
+  RemoveSubmodule dep -> do
+    let dep' = "lib" </> dep
+    putRemove dep'
+    removeDirectoryRecursive (repoDir </> dep')
+    writeRepo repoDir repo
+
+  SidestepLocal dep -> do
+    putSidestep dep
+    removeDirectoryRecursive (repoDir </> dep)
+    _ <- liftIO . runEitherT $ call_ id mafia ["build"]
+    writeRepo repoDir repo
+
+  SidestepSubmodule dep -> do
+    let dep' = "lib" </> dep
+    putSidestep dep'
+    removeDirectoryRecursive (repoDir </> dep')
+    _ <- liftIO . runEitherT $ call_ id mafia ["build"]
+    writeRepo repoDir repo
+
+putAdd :: MonadIO m => Text -> m ()
+putAdd x = liftIO (T.putStrLn ("+ " <> x))
+
+putRemove :: MonadIO m => Text -> m ()
+putRemove x = liftIO (T.putStrLn ("- " <> x))
+
+putSidestep :: MonadIO m => Text -> m ()
+putSidestep x = liftIO (T.putStrLn ("~ " <> x))
+
 ------------------------------------------------------------------------
 
 return []
-tests = $forAllProperties $ quickCheckWithResult (stdArgs {maxSuccess = 10})
+tests = $forAllProperties $ QC.quickCheckWithResult (QC.stdArgs {QC.maxSuccess = 10})
