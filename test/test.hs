d534dd9b308850d9a3d27546f56ff631c09a612b
Jacob Stanley <jacob@stanley.io>
2016-01-29 08:54:19 +1100

    Add cabal configure flags support
    Usage:
    
      mafia build -fmyflag   # myflag on
      mafia build -f+myflag  # myflag on
      mafia build -f-myflag  # myflag off

--- test/test.hs
+++ test/test.hs
@@ -1,6 +1,7 @@
 import           Disorder.Core.Main
 
 import qualified Test.Mafia.Cabal.Dependencies
+import qualified Test.Mafia.Cabal.Types
 import qualified Test.Mafia.Hoogle
 import qualified Test.Mafia.Package
 import qualified Test.Mafia.Process
@@ -9,6 +10,7 @@ main :: IO ()
 main =
   disorderMain [
       Test.Mafia.Cabal.Dependencies.tests
+    , Test.Mafia.Cabal.Types.tests
     , Test.Mafia.Hoogle.tests
     , Test.Mafia.Package.tests
     , Test.Mafia.Process.tests
