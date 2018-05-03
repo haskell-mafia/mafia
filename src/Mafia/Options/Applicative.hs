78f51f68d284f46a661481c4cd0bc3e5dcc5f8e0
George Granberry <george.granberry@gmail.com>
2018-05-03 10:09:25 -0400

    Added cabal version info to the version flag

--- src/Mafia/Options/Applicative.hs
+++ src/Mafia/Options/Applicative.hs
@@ -23,10 +23,13 @@ import qualified Data.Attoparsec.Text as A
 import           Data.String (String)
 import qualified Data.Text as T
 
+import           Distribution.Version (Version, versionNumbers)
+
 import           Options.Applicative as X
 import           Options.Applicative.Types as X
 
 import           Mafia.P
+import           Mafia.Cabal.Version
 
 import           System.Exit (exitSuccess, ExitCode(..), exitWith)
 import           System.IO (IO, putStrLn, print, hPutStrLn, stderr)
@@ -41,6 +44,9 @@ data SafeCommand a =
   | DependencyCommand
   | RunCommand RunType a
   deriving (Eq, Show)
+
+showVersion :: Version -> String
+showVersion = intercalate "." . fmap show . versionNumbers
 
 -- | Turn a parser into a ReadM
 eitherTextReader :: (e -> Text) -> (Text -> Either e a) -> ReadM a
@@ -78,8 +84,14 @@ cli :: Show a => [Char] -> [Char] -> [[Char]] -> Parser a -> (a -> IO b) -> IO b
 cli name v deps commandParser act = do
   dispatch (safeCommand commandParser) >>= \a ->
     case a of
-      VersionCommand ->
-        putStrLn (name <> ": " <> v) >> exitSuccess
+      VersionCommand -> do
+        putStrLn (name <> ": " <> v)
+        eitherCV <- runEitherT getCabalVersion
+        case eitherCV of
+          Right cv ->
+            putStrLn ("built with cabal version: " <> showVersion cv ) >> exitSuccess
+          Left _ ->
+            putStrLn "Unable to retrieve cabal version" >> exitSuccess
       DependencyCommand ->
         mapM putStrLn deps >> exitSuccess
       RunCommand DryRun c ->
